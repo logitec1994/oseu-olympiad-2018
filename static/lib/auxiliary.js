89c099e015ae02113527d532569a03e8c69e303f
logitec1994 <123654978a@gmail.com>
2018-04-30 21:22:47 +0300

    Added:  auxiliary - client library cookies - interaction with cookies
    Added:
    auxiliary - client library
    cookies - interaction with cookies

---
+++ static/lib/auxiliary.js
@@ -0,0 +1,54 @@
+
+JSON.isjson = function(s) {
+    try {
+        JSON.parse(s);
+    } catch (e) {
+        return false;
+    }
+    return true;
+};
+
+String.prototype.cut = function (l) {
+    if (this.length > l) {
+        return this.slice(0, l) + "…";
+    }
+    return this;
+};
+
+String.prototype.toTitleCase = function () {
+    return this.charAt(0).toUpperCase() + this.slice(1);
+};
+
+Array.prototype.rand = function () {
+    return this[Math.floor(Math.random() * (this.length - 1))];
+};
+
+let aux = {
+    rand: function (min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    },
+    range: function (begin, end) {
+        let range = [];
+        for (let i = begin; i < end; ++i) {
+            range.push(i);
+        }
+        return range;
+    },
+    template: function(templateid, data = {}) {
+        return document.getElementById(templateid).innerHTML.replace(/{(\w*)}/g, function(m, key) {
+          return data.hasOwnProperty(key) ? data[key] : "";
+        });
+    }
+};
+
+String.prototype.trim = function () {
+    return String(this).replace(/^[\n\s]+|[\n\s]+$/g, '');
+};
+
+String.prototype.strip = function (s) {
+    if (!s) {
+        throw new Error("Missing first argument!");
+    }
+    let regex = new RegExp("^[{1}]+|[{0}]+$".format(s, s), "gi");
+    return String(this).replace(regex, '');
+};
