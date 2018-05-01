e87932983e7f8f54291aa5d473c85524f1ef4e36
logitec1994 <123654978a@gmail.com>
2018-05-01 20:00:34 +0300

    Re-edit main page
    Re-edit main page

--- templates/main.php
+++ templates/main.php
@@ -1,48 +1,59 @@
 <?php
 include_once 'templates/header.php'
 ?>
-<div class="main grid">
-    <nav class="navigation grid-item1">
-        <ul>
-            <li><a href="/">Главная</a></li>
-            <li><a href="/quests">Квесты</a></li>
-            <li><a href="/about">О нас</a></li>
-        </ul>
-    </nav>
-    <div class="role-buttons grid-item2">
-        <ul>
-            <li><a href="#">Вход</a></li>
-            <li><a href="#">Регистрация</a></li>
-        </ul>
-    </div>
-    <div class="content grid-item3">
-        <div class="logo"></div>
-        <div class="text">
-            <p>Проект "Абитуриент"</p>
-            <p>ОГЭКУ</p>
-            <p>Пройди путь от абитуриента до студента нашего университета</p>
+    <header class="header">
+        <nav class="menu">
+            <ul>
+                <li><a href="/">Главная</a></li>
+                <li><a href="/quests">Квесты</a></li>
+                <li><a href="/authorization">Авторизация</a></li>
+                <li><a href="/about">О нас</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main class="content">
+        <div class="un-disc">
+            <div class="logo-un"></div>
+            <p>
+                Описание универа
+            </p>
         </div>
-        <div class="logo2"></div>
+        <div class="fac-disc">
+            <div class="text">
+                <p>
+                    Описание факультета.
+                    Приймальна комісія Одеського державного екологічного університету працює з 20 червня по 30 вересня 2018 року щоденно без вихідних з 08.50 по 18.30 год.  Прийом документів вступникі вденної та заочної форм навчання здійснюється щоденно з 9.00 по 17.00 год.  Обідня перерва з 13.00 до 13.30 год.
+                </p>
+                <p>
+                    Приймальна комісія ОДЕКУ розташована на 1-му поверсі навчально - лабораторного No 1 за адресою:  м. Одеса,  вул. Львівська, буд. 15, каб. 101
+                </p>
+                <p>
+                    Тел.: (0482) 32-67-64, 32-67-57
+                    Факс: (0482) 32-67-64
+                    E-mail: serbov@odeku.edu.ua, abitur@odeku.edu.ua
+                </p>
+                <p>
+                    Комп’ютерні науки - Інтернет програмування - ЗНО: Українська мова та література Математика Іноземна мова або Фізика. Комп’ютерні науки - Інформаційні управляючі системи та технології - ЗНО: Українська мова та література Математика Іноземна мова або Фізика.
+                </p>
+            </div>
+            <div class="logo-fac"></div>
+        </div>
+        <div class="list-zno">
+            <p>
+                <h2>ЗНО</h2>
+                <ul>
+                    <li>Українська мова та література</li>
+                    <li>Математика</li>
+                    <li>Іноземна мова або Фізика</li>
+                </ul>
+            </p>
+        </div>
+    </main>
+    <div class="msg-wrap">
+        <div class="msg">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium fuga asperiores dolorum animi qui temporibus numquam dolorem a ducimus modi et, quidem nostrum eum, illum magni! Voluptate minima maxime voluptas!
+        </div>
     </div>
-    <div class="form grid-item4">
-        <form action="/registration" method="post" id="registration-form">
-            <p>Регистрация</p>
-            <input type="text" name="lastname" placeholder="Введите фамилию...">
-            <input type="text" name="firstname" placeholder="Введите имя...">
-            <input type="text" name="patronymic" placeholder="Введите отчество...">
-            <input type="email" name="email" placeholder="Ведите E-mail...">
-            <input type="password" name="password" placeholder="Введите пароль...">
-            <input type="password" name="re-password" placeholder="Повторите пароль...">
-            <input type="submit" value="Регистрация" name="registration">
-        </form>
-        <!-- <form action="/login" method="post" id="login-form">
-            <p>Вход</p>
-            <input type="email" name="email" placeholder="Введите E-mail...">
-            <input type="password" name="password" placeholder="Введите пароль...">
-            <input type="submit" value="Вход" name="login">
-        </form> -->
-    </div>
-</div>
 
 <link rel="stylesheet" href="/static/styles/main.css">
 
