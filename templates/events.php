9659af2b91c21b5a53309937d38935f177031c51
Ivan Zatevaxin <zatevaxin.ivan@gmail.com>
2018-05-05 18:54:43 +0300

    Updated events page.

--- templates/events.php
+++ templates/events.php
@@ -2,93 +2,21 @@
 include_once 'templates/header.php'
 ?>
 
-<div class="l-wrapper">
-    <nav class="l-side-nav">
-        <ul class="side-nav">
-            <li class="is-active"><span>Домашние приключения</span></li>
-            <li><span>Сквозь тернии к звездам</span></li>
-            <li><span>Gaudeāmus igĭtur</span></li>
-        </ul>
-    </nav>
-    <ul class="l-main-content main-content">
-        <li class="l-section section section--is-active">
-            <div class="home-adventures">
-                <div class="home-adventures-inner">
-                    <p class="home-adventures-text">
-                        Погрузитесь в атмосферу выбора желанного жизненного пути
-                    </p>
-                    <p class="home-adventures-text">
-                        Позвольте нам познакомить вас с нашим прекрасным <span class="yellow-text"><br>солнечным</span> городом и <span class="green-text">дружным</span> университетом
-                    </p>
-
-                    <div class='proceed-btn'>
-                        <svg class='proceed-btn-border' view-box='0 0 180 60'>
-                            <polyline class='bg-line' points='179,1 179,59 1,59 1,1 179,1'>
-                            </polyline>
-                            <polyline class='hl-line' points='179,1 179,59 1,59 1,1 179,1'>
-                            </polyline>
-                        </svg>
-                        <span class='proceed-btn-text'>Приступить</span>
-                    </div>
-                </div>
-            </div>
-        </li>
-        <li class="l-section section">
-            <div class="to-stars">
-                <div class="to-stars-inner">
-                    <p class="to-stars-text">
-                        Познакомьтесь с будущими преподавателями и наставниками
-                    </p>
-                    <span class="to-stars-text">
-                            Представьте, что именно в данную минуту вам необходимо:
-                            <ul class="to-stars-list">
-                                <li>
-                                    подать все документы на поступление
-                                </li>
-                                <li>
-                                    познать всю суть собеседования
-                                </li>
-                                <li>
-                                    проверить каково это - сдавать экзамены
-                                </li>
-                            </ul>
-                        </span>
-                    <div class='proceed-btn'>
-                        <svg class='proceed-btn-border' view-box='0 0 180 60'>
-                            <polyline class='bg-line' points='179,1 179,59 1,59 1,1 179,1'>
-                            </polyline>
-                            <polyline class='hl-line' points='179,1 179,59 1,59 1,1 179,1'>
-                            </polyline>
-                        </svg>
-                        <span class='proceed-btn-text'>Приступить</span>
-                    </div>
-                </div>
-
-
-            </div>
-        </li>
-        <li class="l-section section">
-            <div class="about">
-                xvxfcv
-
-                <div class='proceed-btn'>
-                    <svg class='proceed-btn-border' view-box='0 0 180 60'>
-                        <polyline class='bg-line' points='179,1 179,59 1,59 1,1 179,1'>
-                        </polyline>
-                        <polyline class='hl-line' points='179,1 179,59 1,59 1,1 179,1'>
-                        </polyline>
-                    </svg>
-                    <span class='proceed-btn-text'>Приступить</span>
-                </div>
-            </div>
-        </li>
-
-    </ul>
-</div>
-
 <link rel="stylesheet" href="/static/styles/events.css">
 <script src="/static/javascript/events.js"></script>
 
+<div class="presentation-wrapper" data-current="home-adventures">
+    <div class="nav-wrapper">
+        <div class="nav-section">
+            <div data-name="home-adventures" class="section-title nav-active"><span>Домашние приключения</span></div>
+            <div data-name="to-the-stars" class="section-title"><span>Сквозь тернии к звездам</span></div>
+            <div data-name="gaudeamus-igitur" class="section-title"><span>Gaudeāmus igĭtur</span></div>
+        </div>
+    </div>
+    <div class="presentation-section named--home-adventures section-active"></div>
+    <div class="presentation-section named--to-the-stars"></div>
+    <div class="presentation-section named--gaudeamus-igitur"></div>
+</div>
 <?php
 include_once 'templates/footer.php'
 ?>
