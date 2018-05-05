dc1bda2da2bdaea8577c2a6ed80153f72a98c519
logitec1994 <123654978a@gmail.com>
2018-05-05 12:03:08 +0300

    change direction chevron

--- static/javascript/main.js
+++ static/javascript/main.js
@@ -1,9 +1,14 @@
 $(function () {
 
     let header = $('header');
+    let chevron = $('div.sidebar-mode-toggle');
 
     header.find('div.sidebar-mode-toggle').on('click', (e) => {
         header.toggleClass("folded");
+    });
+
+    chevron.on('click', (e) => {
+        chevron.find('i').toggleClass("fa-chevron-left fa-chevron-right")
     });
 
 });
