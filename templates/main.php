2b5109af88c4b23c169452c73608677f7608f130
logitec1994 <123654978a@gmail.com>
2018-05-05 21:34:14 +0300

    Added:
    Added to main page background and style for text

--- templates/main.php
+++ templates/main.php
@@ -1,41 +1,33 @@
 <?php
 include_once 'templates/header.php'
 ?>
+    <link rel="stylesheet" href="/static/styles/main.css">
 
     <div class="slider-wrapper">
         <ul class="accordion">
-            <li><img src="/static/resources/images/slider/2.jpg" alt=""></li>
-            <li><img src="/static/resources/images/slider/3.jpg" alt=""></li>
-            <li><img src="/static/resources/images/slider/4.jpg" alt=""></li>
-            <li><img src="/static/resources/images/slider/5.jpg" alt=""></li>
-            <li><img src="/static/resources/images/slider/2.jpg" alt=""></li>
-            <li><img src="/static/resources/images/slider/4.jpg" alt=""></li>
-            <li><img src="/static/resources/images/slider/3.jpg" alt=""></li>
+            <li><img src="/static/resources/images/slider/2.jpg" alt=""><span>Наш универтисет</span></li>
+            <li><img src="/static/resources/images/slider/3.jpg" alt=""><span>Награды университета</span></li>
+            <li><img src="/static/resources/images/slider/1.jpg" alt=""><span>Научное общество Ecoprostir</span></li>
+            <li><img src="/static/resources/images/slider/5.jpg" alt=""><span>Наши выпускники</span></li>
         </ul>
     </div>
 
-    <div class="main-text">
-        Наш ресурс - ознакомительная платформа для абитуриентов\будущих студентов. Мы разрабатали алгоритмы адаптивного ознакомления с электронным оформлением документов для поступления в университет, помогаем выбрать направления и определится со специальностью. С помощью игры в виде квестов, каждый абитуеринт может узнать необходимую информацию для поступление в желаемый вуз.
-        Наша цель - сделать оформление удобным и интерактивным.
+    <div class="main-text__wrapper">
+        <div class="main-text">
+            Наш ресурс - ознакомительная платформа для абитуриентов/будущих студентов. Мы разрабатали алгоритмы адаптивного ознакомления с электронным оформлением документов для поступления в университет, помогаем выбрать направления и определится со специальностью. С помощью игры в виде квестов, каждый абитуеринт может узнать необходимую информацию для поступление в наш вуз.
+        </div>
+        <ul>
+            <li>
+                <span>Событие</span>
+                <div class="list-text">Одно из трёх возможных событий <a href="/events#01">Домашние приключения</a>, <a href="/events#02">Сквозь тернии к звёздам</a> и <a href="/events#03">Gaudeāmus igĭtur</a>, которое вы можете пройти на нашем ресурсе.</div>
+            </li>
+            <li>
+                <span>Квесты</span>
+                <div class="list-text">Квест в реальности, как и любая другая игра, имеет несколько правил. Они направлены на то, чтобы игроки полностью погрузились в историю и получили массу положительных эмоций. </div>
+            </li>
+        </ul>
     </div>
 
-
-    <ul>
-        <li>
-            <span>Игра</span>
-            <div class="list-text">Форма деятельности в условных ситуациях, направленная на воссоздание и усвоение общественного опыта, фиксированного в социально закрепленных способах осуществления предметных действий, в предметах науки и культуры.</div>
-        </li>
-        <li>
-            <span>Событие</span>
-            <div class="list-text">Одно из трёх возможных событий "Домашние приключения", "Сквозь тернии к звёздам" и "Gaudeāmus igĭtur" может стать вашим любимым событием.</div>
-        </li>
-        <li>
-            <span>Квесты</span>
-            <div class="list-text">Квест в реальности, как и любая другая игра, имеет несколько правил. Они направлены на то, чтобы игроки полностью погрузились в историю и получили массу положительных эмоций. </div>
-        </li>
-    </ul>
-
-    <link rel="stylesheet" href="/static/styles/main.css">
 
 <?php
 include_once 'templates/footer.php'
