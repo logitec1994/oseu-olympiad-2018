7d6436f682c37fb81160aa0d7dd83cf5c6661d8f
logitec1994 <123654978a@gmail.com>
2018-05-06 00:41:39 +0300

    Events text and style

--- templates/events.php
+++ templates/events.php
@@ -10,7 +10,35 @@
             <div data-name="gaudeamus-igitur" class="section-title"><span>Gaudeāmus igĭtur</span></div>
         </div>
     </div>
-    <div class="presentation-section named--home-adventures"></div>
-    <div class="presentation-section named--to-the-stars"></div>
-    <div class="presentation-section named--gaudeamus-igitur"></div>
+    <div class="presentation-section named--home-adventures">
+        <div class="home-wrapper section-wrapper">
+            <div class="home-text">
+                <p>Погрузитесь в атмосферу желанного жизненного пути</p>
+                <p>Позвольте нам познакомить вас с нашим университетом</p>
+                <button class="btn btn-success">Приступить</button>
+            </div>
+        </div>
+    </div>
+    <div class="presentation-section named--to-the-stars">
+        <div class="to-the-stars-wrapper section-wrapper">
+            <div class="to-the-stars-text">
+                <p>Понакомтесь с будущими преподавателями и наставниками</p>
+                <p>Представьте, что именно в данную минуту вам необходимо:
+                    <ul>
+                        <li>Подать все документы на поступление</li>
+                        <li>Узнать суть собеседования</li>
+                        <li>Приблизится к экзаменам</li>
+                    </ul>
+                </p>
+                <button class="btn btn-success">Приступить</button>
+            </div>
+        </div>
+    </div>
+    <div class="presentation-section named--gaudeamus-igitur">
+        <div class="gaudeamus-wrapper section-wrapper">
+            <div class="gaudeamus-text">
+                <button class="btn btn-success">Приступить</button>
+            </div>
+        </div>
+    </div>
 </div>
