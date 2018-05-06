858d89f42fd12149263413d695bc2a304946becd
logitec1994 <123654978a@gmail.com>
2018-05-06 22:07:26 +0300

    Added quest 1.1

---
+++ templates/quest.1.1.php
@@ -0,0 +1,20 @@
+<div class="home-event-wrap">
+    <span>Университет
+                    <select name="" id="" disabled>
+                        <option value="oseu">ОГЕКУ</option>
+                    </select>
+                    </span>
+    <span>Факультет
+                        <select name="" id="" disabled>
+                            <option value="fkn">Компьютерные науки</option>
+                        </select>
+                    </span>
+    <span>Кто порекомендовал?
+                        <p>
+                            <label for="friend"><span>Друг: <input type="radio" name="invite" id="friend"></span></label>
+                            <label for="family"><span>Член семьи: <input type="radio" name="invite" id="family"></span></label>
+                            <label for="teacher"><span>Учитель в школе: <input type="radio" name="invite" id="teacher"></span></label>
+                        </p>
+                    </span>
+</div>
+
